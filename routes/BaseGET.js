7a4a0e23d9457f19c88c77e8bd8430231c4998a7
Crawl <icrawltogo@gmail.com>
2017-01-20 23:36:37 +0100

    Update css

--- routes/BaseGET.js
+++ routes/BaseGET.js
@@ -6,7 +6,10 @@ class BaseRouteGET extends Route {
 	}
 
 	async run(ctx, next) {
-		ctx.state = { title: 'Tohru', body: 'Tohru - A blazingly fast pomf-like upload service that doesn\'t suck.' };
+		ctx.state = {
+			title: 'Tohru',
+			subtitle: 'A blazingly fast pomf-like upload service that doesn\'t suck.'
+		};
 		await ctx.render('index.hbs');
 
 		return next();
