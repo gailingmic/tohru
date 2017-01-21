02919da880b37fce5df1f228da9c6df59c8326d8
Crawl <icrawltogo@gmail.com>
2017-01-21 12:30:14 +0100

    Add dropzone

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,50 @@
-document.addEventListener('DOMContentLoaded', function () {
-	function stopDefaultEvent(evt) {
-		evt.stopPropagation();
-		evt.preventDefault();
-	}
+function createDropzoneContainer() {
+	var div = document.createElement('div');
+	div.id = 'dropzone';
 
-	function selectFiles(target, evt) {
-		stopDefaultEvent(evt);
-		target.click();
-	}
+	document.getElementById('uploadContainer').appendChild(div);
 
-	var uploadButton = document.getElementById('upload-btn');
-	var uploadInput = document.getElementById('upload-input');
-	uploadButton.addEventListener('click', selectFiles.bind(this, uploadInput));
-	document.getElementById('upload-form').classList.add('js');
-});
+	initializeDropzone();
+}
+
+function initializeDropzone() {
+	var previewNode = document.querySelector('#uploads');
+	previewNode.id = '';
+	var previewTemplate = previewNode.parentNode.innerHTML;
+	previewNode.parentNode.removeChild(previewNode);
+
+	var dropzone = new Dropzone(document.body, {
+		url: '/upload',
+		paramName: 'files[]',
+		maxFilesize: 250,
+		parallelUploads: 10,
+		uploadMultiple: false,
+		previewsContainer: 'div#upload-filelist',
+		previewTemplate: previewTemplate,
+		createImageThumbnails: false,
+		maxFiles: 1000,
+		autoProcessQueue: true,
+		clickable: ".btn",
+		/* headers: { 'auth': upload.token },*/
+		init: function() {
+			this.on('addedfile', function(file) {
+				document.getElementById('upload-filelist');
+			});
+		}
+	});
+
+	dropzone.on('success', function (file, response) {
+		console.log(response);
+
+
+		var a = document.createElement('a');
+		a.href = 'http://localhost:3000/images/' + response[0].filename;
+		a.target = '_blank';
+		a.innerHTML = 'http://localhost:3000/images/' + response[0].filename;
+		file.previewTemplate.querySelector('.links').appendChild(a);
+	});
+}
+
+window.onload = function() {
+	createDropzoneContainer();
+};
