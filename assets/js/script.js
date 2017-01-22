8f2d1fe32af62ca2d81c33bf275422e02165909d
Crawl <icrawltogo@gmail.com>
2017-01-22 02:22:24 +0100

    Really finish frontpage now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 function initializeDropzone() {
-	var previewNode = document.getElementById('template');
+	var previewNode = document.querySelector('.template');
 	previewNode.id = '';
+	previewNode.className = '';
 	var previewTemplate = previewNode.parentNode.innerHTML;
 	previewNode.parentNode.removeChild(previewNode);
 
@@ -9,35 +10,31 @@ function initializeDropzone() {
 		paramName: 'files[]',
 		maxFilesize: 250,
 		parallelUploads: 10,
+		thumbnailWidth: 60,
+		thumbnailHeight: 60,
 		uploadMultiple: false,
-		previewsContainer: '#uploads',
+		previewsContainer: '#preview',
 		previewTemplate: previewTemplate,
 		createImageThumbnails: false,
 		maxFiles: 1000,
 		autoProcessQueue: true,
-		clickable: '.btn',
-		/* headers: { 'auth': upload.token },*/
-		init: function() {
-			this.on('addedfile', function(file) {
-				document.getElementById('uploads');
-			});
-		}
+		clickable: '.btn'
+	});
+
+	dropzone.on('sending', function(file) {
+		document.querySelector('.file-progress').style.opacity = '1';
 	});
 
 	dropzone.on('success', function(file, response) {
-		if (response.status !== 200) {
+		/* if (response.status !== 200) {
 			var span = document.createElement('span');
 			span.innerHTML = response.description;
-			return file.previewTemplate.querySelector('.link').appendChild(span);
-		}
-
-		var links = document.createElement('a');
-		links.href = 'http://localhost:3000/images/' + response[0].filename;
-		links.target = '_blank';
-		links.innerHTML = 'http://localhost:3000/images/' + response[0].filename;
-
-		file.previewTemplate.querySelector('.links').appendChild(links);
-		file.previewTemplate.querySelector('.progress').style.display = 'none';
+			return file.previewTemplate.querySelector('.links').appendChild(span);
+		}*/
+		file.previewElement.querySelector('.status').classList.add('hidden');
+		file.previewElement.querySelector('.link').classList.remove('hidden');
+		file.previewElement.querySelector('.link-href').setAttribute('href', 'http://localhost:3000/images/' + response[0].filename);
+		file.previewElement.querySelector('.link-href').innerHTML = 'http://localhost:3000/images/' + response[0].filename;
 	});
 }
 
